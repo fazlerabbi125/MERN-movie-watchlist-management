595647c4e608228bf4d297c5b59c62ff8f61a753
Fazle Rabbi Faiyaz <fazle.faiyaz@bjitgroup.com>
2023-01-04 12:23:44 +0600

    Movie-watchlist management v1

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = ({ header }) => {
+    return (
+        <header className="text-center mb-5" >
+            <h1 className="display-5 banner">{header}</h1>
+        </header>
+
+    );
+}
+
+export default Header;
